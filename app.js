64b6108a6a87ce9e53e0279face5cdeeb09ba8b7
Gino Grippo <gino.carlo02@gmail.com>
2023-09-26 17:19:30 -0300

    Mirror Optimized

--- app.js
+++ app.js
@@ -395,16 +395,12 @@ function checkToCleanRow(){
     }
   }
 
-  function changeView() {
+  function Mirror() {
     //em loop até percorrer todas as linha:
-
-    const matrizInvertida = [];
-    
     for(let i = 0; i < (width*(height-1)-2); i+=width)
     {
       //cópia da row
-      const squaresRemoved = squares.slice(228, 240); //linha original
-      console.log(squaresRemoved);
+      const squaresRemoved = squares.slice(width*(height-2), width*(height-2) + width); //linha original
       const squaresRemovedAux = []; //linha invertida
       
       //inverte colunas dessa linha
@@ -424,36 +420,23 @@ function checkToCleanRow(){
         //new col = 0, col 11
         squaresRemovedAux[newCol] = squaresRemoved[col];
       }
-    
-      matrizInvertida.push(squaresRemovedAux);
       
       //adiciona ela no início novamente
-      
-      squares.splice(228, 12)
-      squares = squaresRemovedAux.concat(squares) //paramos aqui, a linha inverte, mas ela aparece no início. Eu quero que ela apareça no final!
-      squares.forEach(cell => grid.appendChild(cell))
-      
-
+      squares.splice(width*(height-2), width);
+      squares = squaresRemovedAux.concat(squares);
+      squares.forEach(cell => grid.appendChild(cell));
     }
-
-    // adição massiva não funcionando:
-    /*
-    squares.splice(0, squares.length)
-    squares = squares.concat(matrizInvertida) 
-    squares.forEach(cell => grid.appendChild(cell))
-    */
-    console.log(matrizInvertida);
   }
   
 
   if(localInverted && !isInverted){ //se esse clean tiver uma linha especial e o modo de jogo não for especial
     isInverted = true; //então agora o modo de jogo será especial
-    changeView();
+    Mirror();
   }
   else{
     if(localInverted && isInverted){ //se esse clean tiver uma linha especial e o modo de jogo for especial
       isInverted = false; //então agora o modo de jogo não será especial
-      changeView();
+      Mirror();
     }
   }
   
@@ -465,33 +448,5 @@ function checkGameOver(){
     clearInterval(timerId); // pausa o timer
   }
 }
-/*
-function changeView(){
-  newSquares = squares;
-  for(let rowRunner = 0; rowRunner < (1 + width * (height-2)); rowRunner+=12){
-    for(let newCol = 10, col = 1; col < 6 ;col++, newCol--){
-      newSquares[newCol + rowRunner] = squares[col + rowRunner];
-      //10 nova = 1 original 
-      //9 nova = 2 original
-      //8 nova = 3 original
-      //7 nova = 4 original
-      //6 nova = 5 original
-      
-    }
-    for(let newCol = 10, col = 1; col < 6 ;col++, newCol--){
-      newSquares[col + rowRunner] = squares[newCol + rowRunner];
-      //1 nova = 10 original
-      //2 nova = 9 original
-      //3 nova = 8 original
-      //4 nova = 7 original
-      //5 nova = 6 original
-      
-    }
-  }
-  console.log(newSquares);
-  squares = newSquares;
-}
-*/
-
 
 })
